b9ce312c2bdf135860f7ee766f59821424dee607
xuanhong.vuong <xuanhong.vuong@ved.com.vn>
2016-11-09 18:25:03 +0700

    code can run grpc

---
+++ app/src/main/java/com/example/vedhn/demogrpc/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vedhn.demogrpc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.vedhn.demofcm.protobuf.MessageGrpc;
+import com.example.vedhn.demofcm.protobuf.Vedfcm;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
